12ec4d35537272df518097e8e19569f09d06bf90
OlegYaschenko <olegyaschenko@gmail.com>
2022-04-22 17:30:33 +0300

    add proptypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from 'components/data/user.json';
-import data from 'components/data/data.json';
-import friends from 'components/data/friends.json';
-import transactions from 'components/data/transactions.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 import { Container } from './App.styled';
 import { Profile } from 'components/Profile/Profile';
